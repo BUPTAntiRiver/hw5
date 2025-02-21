f1bb22a5f094a83553dde4767c632bb003766a0f
Cole Nelson <ctnelson2@wisc.edu>
2025-02-21 11:22:39 -0600

    updt

--- src/components/nav/pages/BadgerBudsLanding.jsx
+++ src/components/nav/pages/BadgerBudsLanding.jsx
@@ -9,7 +9,7 @@ export default function BadgerBudsLanding(props) {
             <Row>
                 <Col xs={12} lg={4} xl={6}>
                     <p>Badger Buddies is a proxy website (built by you!) for cats on <a href="https://www.madisoncatproject.org/">The Madison Cat Project</a>.</p>
-                    <p>This information is current as of <strong>September 30th, 2024.</strong></p>
+                    <p>This information is current as of <strong>February 20th, 2025.</strong></p>
                     <p>Interested in adopting a cat? <a target="_blank" href="https://www.madisoncatproject.org/browse-indoor">See them here!</a></p>
                     <h3>From the Madison Cat Project...</h3>
                     <p>"Madison Cat Project saves cats through partnerships with other shelters and our community. Whether it’s partnering with area shelters to bring in cats who need another chance or supporting farmers and other folks who need help managing their outdoor cat colonies by offering affordable spay/neuter surgeries and rehoming options, we help hundreds of cats (and people) each year!"</p>
